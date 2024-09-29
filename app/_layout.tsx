ed1af6e4ba1d86fa84b4967f0534c2ec5781c920
minseong <kwan0324@naver.com>
2024-09-30 08:43:23 +0900

    Feat: add eas OTA, refactor logics

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,7 +9,7 @@ import {
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import {
   useColorScheme,
   StyleSheet,
@@ -18,8 +18,11 @@ import {
   ToastAndroid,
   BackHandler,
   TextInput,
+  Alert,
+  Text,
 } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import * as Updates from 'expo-updates';
 
 export { ErrorBoundary } from 'expo-router';
 
@@ -38,27 +41,27 @@ export default function RootLayout(): JSX.Element | null {
   const [exitApp, setExitApp] = useState<boolean>(false);
   const timerRef = useRef<NodeJS.Timeout | null>(null);
 
-  useEffect(() => {
-    const backAction = (): boolean => {
-      if (!exitApp) {
-        setExitApp(true);
-        ToastAndroid.show(
-          '뒤로가기 버튼을 한번 더 누르시면 종료되요.',
-          ToastAndroid.SHORT,
-        );
-        timerRef.current = setTimeout(() => {
-          setExitApp(false);
-        }, 2000); // 2초 내에 다시 누르지 않으면 초기화
-        return true;
-      } else {
-        if (timerRef.current) {
-          clearTimeout(timerRef.current);
-        }
-        BackHandler.exitApp(); // 앱 종료
-        return true;
+  const backAction = useCallback(() => {
+    if (!exitApp) {
+      setExitApp(true);
+      ToastAndroid.show(
+        '뒤로가기 버튼을 한번 더 누르시면 종료되요.',
+        ToastAndroid.SHORT,
+      );
+      timerRef.current = setTimeout(() => {
+        setExitApp(false);
+      }, 2000);
+      return true;
+    } else {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
       }
-    };
+      BackHandler.exitApp();
+      return true;
+    }
+  }, [exitApp]);
 
+  useEffect(() => {
     const backHandler = BackHandler.addEventListener(
       'hardwareBackPress',
       backAction,
@@ -70,7 +73,28 @@ export default function RootLayout(): JSX.Element | null {
         clearTimeout(timerRef.current);
       }
     };
-  }, [exitApp]);
+  }, [backAction]);
+
+  async function checkForUpdates() {
+    if (__DEV__) {
+      console.log('개발 환경에서는 업데이트를 체크하지 않습니다.');
+      return;
+    }
+
+    try {
+      const update = await Updates.checkForUpdateAsync();
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      console.error('업데이트 체크 중 오류 발생:', error);
+    }
+  }
+
+  useEffect(() => {
+    checkForUpdates();
+  }, []);
 
   useEffect(() => {
     if (error) throw error;
