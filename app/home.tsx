4a9b98a23756dbff0623f5cd0d72e16d363f8d4b
minseong <kwan0324@naver.com>
2024-10-10 19:10:17 +0900

    Fix: delete loading ui, change adaptive icon

--- app/home.tsx
+++ app/home.tsx
@@ -1,15 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { WebView } from 'react-native-webview';
 import { Platform, StyleSheet } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
 import NetworkErrorScreen from '@/components/NetworkErrorScreen';
 import { WEB_URL } from '@/constants/webUrl';
-import useWebViewLoader from '@/hooks/useWebViewLoader';
-import FallbackUI from '@/components/FallbackUI';
 
 export default function Home() {
   const { isConnected } = useLocalSearchParams();
-  const { loading, handleLoadStart, handleLoadEnd } = useWebViewLoader();
 
   if (isConnected === 'false') {
     return <NetworkErrorScreen />;
@@ -17,7 +14,6 @@ export default function Home() {
 
   return (
     <>
-      {loading && <FallbackUI />}
       <WebView
         textZoom={100}
         javaScriptEnabled={true}
@@ -25,8 +21,6 @@ export default function Home() {
         source={{ uri: WEB_URL }}
         style={styles.webview}
         allowsBackForwardNavigationGestures={Platform.OS === 'ios'}
-        onLoadStart={handleLoadStart}
-        onLoadEnd={handleLoadEnd}
       />
     </>
   );
--- constants/webUrl.ts
+++ constants/webUrl.ts
@@ -1,1 +1,2 @@
 export const WEB_URL = 'https://recipic.co.kr';
+//export const WEB_URL = 'http://192.168.0.182:5174/test' // 테스트 페이지
